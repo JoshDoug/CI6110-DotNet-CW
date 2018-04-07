cbec3e22de003981fe2deb7e84e5aa2237a5bcf4
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-04-07 15:32:21 +0100

    Initialise project

---
+++ AcademicInformationService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AcademicInformationService.Startup))]
+namespace AcademicInformationService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
