4510eb9e86a779c2f5753a00be8874a6e87af78a
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-04-10 17:27:23 +0100

    Remove postalcode regex in favor of data annotation

--- AcademicInformationService/Models/Address.cs
+++ AcademicInformationService/Models/Address.cs
@@ -23,9 +23,6 @@ namespace AcademicInformationService.Models
         [Display(Name = "County (optional)")] // Following UK Gov Standards
         public string County { get; set; }
 
-        // https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Validation
-        // Using the UK Gov Postal Code Regex Standard available at: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/488478/Bulk_Data_Transfer_-_additional_validation_valid_from_12_November_2015.pdf
-        //[RegularExpression("^ ([Gg][Ii][Rr] 0[Aa]{2})|((([A - Za - z][0 - 9]{1,2})|(([A - Za - z][A - Ha - hJ - Yj - y][0 - 9]{1,2})|(([A - Za - z][0 - 9][A - Za - z])|([A - Za - z][A - Ha - hJ - Yj - y][0 - 9]?[A - Za - z])))) [0-9] [A-Za-z]{2})$")]
         [Required]
         [DataType(DataType.PostalCode)]
         public string Postcode { get; set; }
