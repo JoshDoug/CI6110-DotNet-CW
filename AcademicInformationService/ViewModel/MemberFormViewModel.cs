b1056e4d86b4a7539f8f07a493e628d0e488fd88
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-04-10 18:10:43 +0100

    Add Member View Model

---
+++ AcademicInformationService/ViewModel/MemberFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AcademicInformationService.Models;
+
+namespace AcademicInformationService.ViewModel
+{
+    public class MemberFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public IEnumerable<Gender> Genders { get; set; }
+        public Member Member { get; set; }
+    }
+}
