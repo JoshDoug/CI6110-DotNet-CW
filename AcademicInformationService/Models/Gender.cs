0248a57483b6e510b6c9477b2b6814196f19a483
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-04-10 18:09:45 +0100

    Add Gender model

---
+++ AcademicInformationService/Models/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AcademicInformationService.Models
+{
+    public class Gender
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+    }
+}
