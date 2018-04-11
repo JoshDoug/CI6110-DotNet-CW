ac085429727c3be9f387d1e5639babb80b9c2a5a
Joshua Stringfellow <joshdougstringfellow@gmail.com>
2018-04-11 17:37:12 +0100

    Add Chairs to Home page, cleanup default pages

--- AcademicInformationService/Controllers/HomeController.cs
+++ AcademicInformationService/Controllers/HomeController.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AcademicInformationService.Models;
 
 namespace AcademicInformationService.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var members = _context.Members.Where(m => m.MembershipType.Name == "Chair" || m.MembershipType.Name == "Co-Chair").Include(m => m.MembershipType).ToList();
+            return View(members);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
